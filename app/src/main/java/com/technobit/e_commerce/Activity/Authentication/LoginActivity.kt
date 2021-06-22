e48428fb901c672628e75df12971a27eb3ad07bb
Naufalwidhi <naufalwidhi1@gmail.com>
2021-06-22 09:38:53 +0700

    Login UI

---
+++ app/src/main/java/com/technobit/e_commerce/Activity/Authentication/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.technobit.e_commerce.Activity.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.technobit.e_commerce.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
